ab01e207a87bf62f6c3164b205fc1bc6c037a821
FTurleque <fabrice.turleque@gmail.com>
2022-04-09 18:03:41 +0200

    Qestion 3 part 2

--- js/App.js
+++ js/App.js
@@ -4,12 +4,33 @@ const fetchData = () => fetch('/data/data.json')
 
 
 class Movie {
+    constructor(data) {
+        this._data = data
+    }
+
+    get id() {
+        return this._data.id
+    }
+
+    get title() {
+        return this._data.title
+    }
+
+    get img() {
+        return this._data.img
+    }
+
+    get rating() {
+        return this._data.rating
+    }
 }
 
 
 class App {
     async init() {
         const data = await fetchData()
+        const movies = new Movie(data)
+        console.log(data);
     }
 }
 
