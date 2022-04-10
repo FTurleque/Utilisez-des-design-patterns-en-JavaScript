07fde9eafced3d5bee7f9ea3a83cef1f52897294
FTurleque <fabrice.turleque@gmail.com>
2022-04-10 09:43:02 +0200

    Exercice 5 part 2

---
+++ js/factories/TemplateCardFactory.js
@@ -0,0 +1,11 @@
+class TemplateCardFactory {
+    constructor(data) {
+        if (data.type === 'movie') {
+            return new MovieCard(data)
+        } else if (data.type === 'tv-show') {
+            return new TvShowCard(data)
+        } else {
+            throw "Unknown type"
+        }
+    }
+ }
